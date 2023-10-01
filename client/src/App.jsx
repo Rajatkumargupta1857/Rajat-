4f56e1c82d25ffaf05eb7946af8c8686278848b0
Rajat Kumar Gupta <thiyodor7@gmail.com>
2023-10-02 02:53:22 +0530

    Create Pages Ans Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import { BrowserRouter ,Routes, Route, Link } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './Pages/About';
+import SignIn from './pages/SignIn';
+import SignUp from './Pages/SignUp';
+import Profile from './pages/Profile';
+
 
 function App() {
   return (
-    <h1 className='text-red-500'>App</h1>
+    <>
+       <BrowserRouter>
+     
+                <Routes>
+                        <Route path='/' element={<Home />}/>
+                        <Route path='/About' element={<About />}/>
+                        <Route path='/Sign-In' element={<SignIn />}/>
+                        <Route path='/Sign-Up' element={<SignUp />}/>
+                        <Route path='/Profile' element={<Profile />}/>
+                </Routes>
+
+      </BrowserRouter>
+    </>
+    
   )
 }
 
